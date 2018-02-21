adca8aa62187262957e72679be36d45aa06b1ab0
Igor Timoshenkov <igor.timoshenkov@campoint.net>
2018-02-21 15:30:32 +0100

    getActiveAbos: finish models, factory

--- src/VXPay/Message/Actions/VXPayActiveAbosMessage.js
+++ src/VXPay/Message/Actions/VXPayActiveAbosMessage.js
@@ -1,4 +1,5 @@
 import VXPayMessage from './../../VXPayMessage'
+import VXPayAbo     from './../../Model/VXPayAbo'
 
 class VXPayActiveAbosMessage extends VXPayMessage {
 	constructor() {
@@ -7,18 +8,48 @@ class VXPayActiveAbosMessage extends VXPayMessage {
 	}
 
 	/**
-	 * @todo finish!
+	 * @param {VXPayAbo} abo
+	 */
+	add(abo) {
+		this.abos.push(abo);
+	}
+
+	/**
 	 * @param {Object} data
 	 * @return {VXPayActiveAbosMessage}
 	 */
 	static fromData(data = VXPayActiveAbosMessage.SAMPLE_DATA) {
 		const instance = new VXPayActiveAbosMessage;
+
+		// instantiate models
+		Object.keys(data).forEach(key => {
+			const model      = new VXPayAbo();
+			model.amount     = data[key].amount;
+			model.endDate    = data[key].endDate;
+			model.isActive   = data[key].isActive;
+			model.isFreeAbo  = data[key].isFreeAbo;
+			model.isPaidAbo  = data[key].isPaidAbo;
+			model.isTrialAbo = data[key].isTrialAbo;
+			model.name       = data[key].name;
+
+			// append
+			instance.add(model);
+		});
+
 		return instance;
 	}
 }
 
 VXPayActiveAbosMessage.SAMPLE_DATA = {
-
+	name: {
+		amount:     0,
+		endDate:    {},
+		isActive:   true,
+		isFreeAbo:  false,
+		isPaidAbo:  true,
+		isTrialAbo: true,
+		name:       'Abo'
+	}
 };
 
 export default VXPayActiveAbosMessage;
--- src/VXPay/Model/VXPayAbo.js
+++ src/VXPay/Model/VXPayAbo.js
@@ -1,6 +1,110 @@
 class VXPayAbo {
 	constructor() {
+		this._amount = 0;
+		this._endDate = {};
+		this._isActive = false;
+		this._isFreeAbo = false;
+		this._isPaidAbo = true;
+		this._isTrialAbo = true;
+		this._name = '';
+	}
 
+	/**
+	 * @return {Number}
+	 */
+	get amount() {
+		return this._amount;
+	}
+
+	/**
+	 * @param {Number} value
+	 */
+	set amount(value) {
+		this._amount = value;
+	}
+
+	/**
+	 * @return {{}|*}
+	 */
+	get endDate() {
+		return this._endDate;
+	}
+
+	/**
+	 * @param {{}|*} value
+	 */
+	set endDate(value) {
+		this._endDate = value;
+	}
+
+	/**
+	 * @return {Boolean}
+	 */
+	get isActive() {
+		return this._isActive;
+	}
+
+	/**
+	 * @param {Boolean} value
+	 */
+	set isActive(value) {
+		this._isActive = value;
+	}
+
+	/**
+	 * @return {Boolean}
+	 */
+	get isFreeAbo() {
+		return this._isFreeAbo;
+	}
+
+	/**
+	 * @param {Boolean} value
+	 */
+	set isFreeAbo(value) {
+		this._isFreeAbo = value;
+	}
+
+	/**
+	 * @return {Boolean}
+	 */
+	get isPaidAbo() {
+		return this._isPaidAbo;
+	}
+
+	/**
+	 * @param {Boolean} value
+	 */
+	set isPaidAbo(value) {
+		this._isPaidAbo = value;
+	}
+
+	/**
+	 * @return {Boolean}
+	 */
+	get isTrialAbo() {
+		return this._isTrialAbo;
+	}
+
+	/**
+	 * @param {Boolean} value
+	 */
+	set isTrialAbo(value) {
+		this._isTrialAbo = value;
+	}
+
+	/**
+	 * @return {String}
+	 */
+	get name() {
+		return this._name;
+	}
+
+	/**
+	 * @param {String} value
+	 */
+	set name(value) {
+		this._name = value;
 	}
 }
 
